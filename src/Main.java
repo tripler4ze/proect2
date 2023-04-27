b65a9c1e2bd6bcc1bd0b14cf7b3967d279c57daf
tripler4ze <buklovskiy00@mail.ru>
2023-04-27 18:05:50 +0300

    first commit

---
+++ src/Main.java
@@ -0,0 +1,16 @@
+
+import javax.sound.midi.Soundbank;
+import java.util.Scanner;
+
+
+    public class Main {public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Выедите число");
+        int[] nums = new int[4];
+        nums[0] = scanner.nextInt();
+        nums[1] = scanner.nextInt();
+        nums[2] = scanner.nextInt();
+        nums[3] = scanner.nextInt();
+
+
+    }}
